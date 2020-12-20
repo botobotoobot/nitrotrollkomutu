747a222208dcfaf81eea05988f25d93da30ccd4a
BonnieBotCreator <74361674+botobotoobot@users.noreply.github.com>
2020-12-20 14:49:00 +0300

    Add files via upload

---
+++ nitro.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
+exports.run = function(client, message) {
+  
+  let prefix  = ayarlar.prefix
+
+const yardım = new Discord.MessageEmbed()
+.setColor('RANDOM')
+.setAuthor(`Bonnie`)
+.setDescription(`
+
+
+Nitromu İstiyorsun? <:diaboost:777644355502604318> \n\n||Gerçekten Bu Komutun Nitro Vereceğini Sandıysan Helal Olsun :D||
+
+`)
+.setImage("https://cdn.discordapp.com/attachments/740871896614043669/748878433840398367/Baslksz-1.png")
+message.channel.send(yardım)
+
+  
+   
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: ['Niro'], 
+  permLevel: 0
+};
+
+exports.help = {
+  name: "nitro",
+  description: 'troll',
+  usage: ''
+};
